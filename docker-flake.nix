e70853134765c7a4ebf64074cb655a9c080ffb91
Adam-D-Lewis <>
2022-01-25 08:49:00 -0600

    initial commit

--- docker-flake.nix
+++ docker-flake.nix
@@ -1,8 +1,10 @@
-{ pkgs ? import <nixpkgs> { system = "x86_64-linux";}; 
-  tt_flake ? import /home/balast/CodingProjects/nix/nix-flakes/timetagger_flake/flake.nix {}
-}:  # nixpkgs package set
-pkgs.dockerTools.buildLayeredImage { # helper to build Docker image          
-  name = "nix-hello";                # give docker image a name              
-  tag = "latest";                    # provide a tag                         
-  contents = [ tt_flake.time_tagger ];         # packages in docker image              
+{ pkgs, itemdb }:    
+    
+pkgs.dockerTools.buildLayeredImage {    
+  name = "itemdb";    
+  tag = "latest";    
+  contents = [ itemdb.defaultPackage.x86_64-linux pkgs.bash pkgs.lsd pkgs.vim pkgs.findutils pkgs.gnugrep ];    
+  config = {    
+    Cmd = [ "bash" ];    
+  };    
 }
--- flake.nix
+++ flake.nix
@@ -7,28 +7,13 @@
     itemdb.url = "/home/balast/CodingProjects/nix/nix-flakes/itemdb_flake/";    
   };    
     
-  outputs = inputs@{ self, nixpkgs, itemdb}: rec {    
+  outputs = inputs@{ self, nixpkgs, itemdb }: rec {    
+    legacyPackages.x86_64-linux = rec { 
         pkgs = import nixpkgs { system = "x86_64-linux"; };    
-        # itemdb_pkg = import itemdb {};    
         pypkgs = pkgs.python3Packages;    
-        timetagger = pypkgs.buildPythonPackage {    
-          name = "timetagger";    
-          src = pkgs.fetchFromGitHub {        
-            owner = "almarklein";        
-            repo = "timetagger" ;        
-            rev = "e3526da61c32276d315056483f6b3cfa03d4b657";        
-            sha256 = "6PhBYYrGtAel7bA0q2RM+XxMUnKUhaCA1YvakGRpSk8=";        
-          };        
-          propagatedBuildInputs = [     
-            pypkgs.uvicorn     
-            # asgineer # not in nixpkgs    
-            itemdb # not in nixpkgs, but this has no dependencies so should be easy to package
-            # pscript # also not in nixpkgs, but pure python and no dependencies    
-            pypkgs.pyjwt     
-            pypkgs.jinja2     
-            pypkgs.markdown     
-          ];    
-      };    
-      defaultPackage.x86_64-linux = self.timetagger;    
+        docker-flake = import ./docker-flake.nix { inherit pkgs itemdb; };
+    };
+    
+    defaultPackage.x86_64-linux = legacyPackages.x86_64-linux.docker-flake;
   };    
 } 
