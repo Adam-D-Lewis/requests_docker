b85f1af5fe88d5c60449524b47fc4580816be81b
Adam-D-Lewis <>
2022-01-26 00:46:03 -0600

    working overlay

--- docker-flake.nix
+++ docker-flake.nix
@@ -1,9 +1,15 @@
 { pkgs }:    
-    
+let 
+  my-python-packages-function = python-packages: with python-packages; [
+    requests
+    itemdb
+  ];
+  python-with-my-packages = pkgs.python3.withPackages my-python-packages-function; # pythonPackages.python3.withPackages my-python-pakcages-function; 
+in
 pkgs.dockerTools.buildLayeredImage {    
   name = "requests_image";    
   tag = "latest";    
-  contents = [ pkgs.python3Packages.requests pkgs.bash pkgs.lsd pkgs.vim pkgs.findutils pkgs.gnugrep ];    
+  contents = [ pkgs.bash pkgs.lsd pkgs.vim pkgs.findutils pkgs.gnugrep pkgs.coreutils python-with-my-packages];    
   config = {    
     Cmd = [ "bash" ];    
   };    
--- flake.nix
+++ flake.nix
@@ -1,13 +1,28 @@
-{    
+{   
   description = "A very basic flake";    
     
   inputs = {        
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";        
+    my_itemdb.url = "/home/balast/CodingProjects/nix/nix-flakes/itemdb_flake";
   };    
     
-  outputs = inputs@{ self, nixpkgs }: rec {    
+  outputs = inputs@{ self, nixpkgs, my_itemdb }: rec {    
+    # ref: https://github.com/NixOS/nixpkgs/issues/26487#issuecomment-307363295
+    my_overlay = self: super:
+      rec {
+        # nix-shell -p python.pkgs.my_stuff
+        python3 = super.python3.override {
+          # Careful, we're using a different self and super here!
+          packageOverrides = self: super: {
+            itemdb = super.flask; #super.buildPythonPackage rec {
+            };
+          };
+        };
+      # nix-shell -p pythonPackages.my_stuff
+      # pythonPackages = python.pkgs;
+
     legacyPackages.x86_64-linux = rec { 
-        pkgs = import nixpkgs { system = "x86_64-linux"; };    
+        pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ my_overlay ]; };    
         docker-flake = import ./docker-flake.nix { inherit pkgs; };
     };
     
